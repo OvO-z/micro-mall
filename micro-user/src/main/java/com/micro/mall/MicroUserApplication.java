9a74593d8baf19898a581b87dbfc4d5bdda8301b
QAQ <2436781725@qq.com>
2021-05-17 21:55:21 +0800

    -初始化micro-user模块

---
+++ micro-user/src/main/java/com/micro/mall/MicroUserApplication.java
@@ -0,0 +1,16 @@
+package com.micro.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户模块启动类
+ * @author QAQ
+ * @date 2021/5/17
+ */
+@SpringBootApplication
+public class MicroUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroUserApplication.class, args);
+    }
+}
