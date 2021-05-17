325e1ec2086aebcaac0973594c8bb1d1885c8d67
QAQ <2436781725@qq.com>
2021-05-17 14:53:44 +0800

    - 添加swagger 注释

--- micro-product/src/main/java/com/micro/mall/config/SwaggerConfig.java
+++ micro-product/src/main/java/com/micro/mall/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
+ * Swagger API 文档配置
  * @author QAQ
  * @date 2021/5/17
  */
