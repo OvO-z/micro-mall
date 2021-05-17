e43943603cab28385c47bf26e9629c85c8adeab9
QAQ <2436781725@qq.com>
2021-05-17 22:02:46 +0800

    -添加Mybatis 配置 -添加Swagger 基础配置

---
+++ micro-user/src/main/java/com/micro/mall/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.micro.mall.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.micro.mall.common.config.BaseSwaggerConfig;
+import com.micro.mall.common.domain.SwaggerProperties;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger API 文档配置
+ * @author QAQ
+ * @date 2021/5/17
+ */
+
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class SwaggerConfig extends BaseSwaggerConfig {
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.micro.mall.controller")
+                .title("micro-user 服务")
+                .description("micro-user 相关接口文档")
+                .contactName("QAQ")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+}
